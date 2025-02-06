a8c01e5c31172c47b7d881282e7e0f2866646451
A23AlbertoGC <a23albertogc@iessanclemente.net>
2025-02-06 13:59:51 +0100

    RandomWord done (falta pulir el código)

--- src/main/java/org/example/RandomWordClient.java
+++ src/main/java/org/example/RandomWordClient.java
@@ -11,6 +11,7 @@ public class RandomWordClient {
 
     public static void main(String[] args) {
     byte [] buffer = new byte[1024];
+    byte [] bufferRespuesta = new byte[1024];
     try {
         InetAddress serverAdress = InetAddress.getByName("localhost");
         DatagramSocket socketUDP = new DatagramSocket();
@@ -22,7 +23,7 @@ public class RandomWordClient {
         DatagramPacket paquete = new DatagramPacket(buffer, buffer.length, serverAdress, PORT);
         socketUDP.send(paquete);
 
-        DatagramPacket respuesta = new DatagramPacket(buffer, buffer.length);
+        DatagramPacket respuesta = new DatagramPacket(bufferRespuesta, bufferRespuesta.length);
         socketUDP.receive(respuesta);
         String palabra = new String(respuesta.getData(), 0, respuesta.getLength(), "UTF-8");
         System.out.println(palabra);
